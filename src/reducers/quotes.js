d065da5347c0d9d46be4116ff5374bd81c416c7d
Doug Jones <DSJR1284@GMAIL.COM>
2022-01-07 13:17:07 -0500

    commit tree a9faf113524f6705e37cbc479ae7b966c77a9f54

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,33 @@
+import { downvoteQuote } from "../actions/quotes";
+
 export default (state = [], action) => {
-  return state;
+  switch(action.type){
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      const newQuotes = state.filter(q => q.id !== action.quoteId)
+      return newQuotes;
+    case "UPVOTE_QUOTE": 
+        let index = state.findIndex(q => q.id === action.quoteId)        
+        const newQuote = {...state[index], 
+          votes: state[index].votes + 1
+        }
+      return [
+        ...state.slice(0, index),
+        newQuote,
+        ...state.slice(index + 1)
+      ];
+      case "DOWNVOTE_QUOTE": 
+        index = state.findIndex(q => q.id === action.quoteId)        
+        const downQuote = {...state[index]}
+        if (downQuote.votes > 0){        
+      return [
+        ...state.slice(0, index),
+        downQuote,
+        ...state.slice(index - 1)
+      ]
+    }      
+      default:  
+        return state;
+  }
 }
